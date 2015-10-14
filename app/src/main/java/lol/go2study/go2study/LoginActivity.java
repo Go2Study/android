c1ecf45ba3c797f430905eebfe35fb8cf9ce85ea
Todor Kosev <t.kosev@student.fontys.nl>
2015-10-14 15:53:20 +0200

    Access token saved

--- app/src/main/java/lol/go2study/go2study/LoginActivity.java
+++ app/src/main/java/lol/go2study/go2study/LoginActivity.java
@@ -1,18 +1,37 @@
 package lol.go2study.go2study;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 
-public  class LoginActivity extends Activity {
+public  class LoginActivity extends Activity
+{
 
 
     Intent browserIntent;
@@ -28,30 +47,53 @@ public  class LoginActivity extends Activity {
 
         settings = new OAuthSettings();
 
-        browserIntent  = new Intent(Intent.ACTION_VIEW, settings.getRequestUri());
-
-
+        browserIntent = new Intent(Intent.ACTION_VIEW, settings.getRequestUri());
         final Button login = (Button) findViewById(R.id.buttonLogin);
-
-        login.setOnClickListener(new View.OnClickListener() {
-            int counter = 0;
-
+        login.setOnClickListener(new View.OnClickListener()
+        {
             public void onClick(View v) {
-                counter++;
                 login.setText("hi");
                 startActivity(browserIntent);
 
-                //setContentView(R.layout.activity_people);
-                Log.v("ACCESS TOKEN::::  ", "dadad");
-                System.out.print("Todor Sksks");
-                //login.setText(counter++);
-                // Do something in response to button click
             }
         });
 
+        try
+        {
+            Uri uri = getIntent().getData();
+            settings.SaveAccessToken(uri);
+          //  makeGetRequest();
 
-        //
+        }
+        catch (NullPointerException e)
+        {
+            Log.v("mess :", e.getMessage());
+        }
     }
 
+    private void makeGetRequest()
+    {
+        try
+        {
+            HttpClient client = new DefaultHttpClient();
+            String url = settings.getBaseUrl().concat( settings.getPeopleEndPoint());
+            HttpGet get = new HttpGet(url);
+            //get.setHeader();
+            get.addHeader("Authorization", "Bearer " + settings.getAccess_Token());
+            HttpResponse responseGet = client.execute(get);
+            HttpEntity resEntityGet = responseGet.getEntity();
+            if (resEntityGet != null)
+            {
+                // do something with the response
+                String response = EntityUtils.toString(resEntityGet);
+                Log.i("GET RESPONSE", response);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+       // Log.d("Response of GET request", response.toString());
+    }
 
 }
--- app/src/main/java/lol/go2study/go2study/OAuthSettings.java
+++ app/src/main/java/lol/go2study/go2study/OAuthSettings.java
@@ -1,8 +1,12 @@
 package lol.go2study.go2study;
 
 import android.net.Uri;
+import android.util.Log;
 
 import java.net.URI;
+import java.net.URL;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 /**
  * Created by Ashish on 12/10/15.
@@ -11,22 +15,76 @@ public class OAuthSettings
 {
     private  String clientID ;
     private  String requestedScopes ;
-    private  String redirectUrl;
+    private  String callbackURL;
+    private  String oauthURL;
+    private  String accessToken;
+    private  String baseUrl;
+    private  String peopleEndPoint;
 
     public OAuthSettings()
     {
         clientID = "i271628-go2study-implicit";
         requestedScopes = "fhict";
-        redirectUrl = "go2study://oauth/authorize";
+        callbackURL = "go2study://oauth/authorize";
+        oauthURL = "https://tas.fhict.nl/identity/connect/authorize?client_id="+clientID+ "&scope="+ requestedScopes + "&response_type=token&redirect_uri="+ callbackURL;
+        baseUrl = "https://tas.fhict.nl:443/api/v1/";
+        peopleEndPoint = "people";
+    }
 
+    public String getPeopleEndPoint()
+    {
+        return peopleEndPoint;
     }
 
     public Uri getRequestUri()
     {
-        String uri = "https://tas.fhict.nl/identity/connect/authorize?client_id="+clientID+ "&scope="+ requestedScopes + "&response_type=token&redirect_uri="+ redirectUrl;
-        return Uri.parse(uri);
+        return Uri.parse(oauthURL);
     }
 
+    public String getsClientID()
+    {
+        return  clientID;
+    }
+
+    public String getBaseUrl()
+    {
+        return baseUrl;
+    }
+
+    public String getAccess_Token()
+    {
+        return accessToken;
+
+    }
+    private void setAccessToken(String token)
+    {
+        accessToken = token;
+
+    }
+
+    public void SaveAccessToken(Uri uri)
+    {
+       // accessToken = token;
+        if (uri != null)
+        {
+
+            String[] temp = String.valueOf(uri).split("#");
+            String[] urlComponents = temp[1].split("&");
+            Dictionary<String, String> dictionary = new Hashtable<>();
+            for(String s: urlComponents)
+            {
+                String[] pairComponents = s.split("=");
+                String key = pairComponents[0];
+                String value = pairComponents[1];
+                dictionary.put(key,value);
+            }
+            setAccessToken(dictionary.get("access_token"));
+            Log.v("Access_Token:::::", getAccess_Token());
+            //Store Access Token
+            //  settings.SaveAccessToken(dictionary.get("access_token"));
+            //    Log.v("Access_token", settings.getAccess_Token());
+        }
+    }
 
 
 }
